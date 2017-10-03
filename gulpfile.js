f4b4469240479d973816df2599c122bd996b2849
Mariam Sleiman <maaar.iam94@gmail.com>
2017-10-03 02:03:18 -0300

    npm

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,62 @@ var browserSync = require('browser-sync').create();
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
+var notify = require('gulp-notify');
+var sourcemaps = require('gulp-sourcemaps');
+var styleLint = require('gulp-stylelint');
+var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+var styleLintFixer = require('gulp-stylefmt');
 
+
+gulp.task('css-lint-fixer', function() {
+  return gulp.src('scss/*.scss')
+    .pipe(styleLintFixer({config :{
+      "extends": "stylelint-config-sass-guidelines",
+      "rules": {
+        "indentation": false,
+        "number-leading-zero": null
+      }
+    }}))
+    .pipe(gulp.dest('scss'));
+});
+
+
+gulp.task('css-lint', ['css-lint-fixer'], function() {
+  return gulp.src('scss/*.scss')
+    .pipe(styleLint({
+        reporters: [
+          {formatter: 'string', console: true}
+        ],
+        config :{
+          "extends": "stylelint-config-sass-guidelines",
+          "rules": {
+            "indentation": false,
+            "number-leading-zero": null
+          }
+        }
+      }));
+});
 
 // Compiles SCSS files from /scss into /css
 gulp.task('sass', function() {
   return gulp.src('scss/*.scss')
-    .pipe(sass())
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.css'))
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }).on('error', notify.onError({
+        title: 'Sass Error',
+        subtitle: [
+          '<%= error.relativePath %>',
+          '<%= error.line %>'
+        ].join(':'),
+        message: '<%= error.messageOriginal %>',
+        open: 'file://<%= error.file %>',
+        onLast: true
+    })))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer())
     .pipe(gulp.dest('css'))
     .pipe(browserSync.reload({
       stream: true
